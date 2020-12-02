81076d6063027334437c358614714f4bcb47346d
am-arth <72318136+am-arth@users.noreply.github.com>
2020-12-02 22:12:32 +0300

    Add files via upload

---
+++ lesson1/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {	
+    public static void main(String[] args) {
+       int cpu = 1;
+       int core = 4;
+       int frequence = 1700;
+       String CpuName = "i5-3317";
+       
+       System.out.println ("Тип Вашего процессора " + CpuName);
+       System.out.println ("У Вас " + cpu + " процессор");
+       System.out.println ("Частота Вашнго процессора " + frequence); 
+       System.out.println ("У Вашего процессора " + core + " ядер");
+    }
+}
