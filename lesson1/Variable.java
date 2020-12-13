1ab5b4e263486c28a4694e09465d0dd013b3f9cd
am-arth <72318136+am-arth@users.noreply.github.com>
2020-12-13 21:46:32 +0300

    Add files via upload

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -2,12 +2,22 @@ public class Variable {
     public static void main(String[] args) {
        int cpu = 1;
        int core = 4;
-       int frequence = 1700;
+       double frequence = 1.700;
        String CpuName = "i5-3317";
-       
-       System.out.println ("Тип Вашего процессора " + CpuName);
+       char energy = 'U';
+	   long capacity = 976759996;
+	   byte bus = 64;
+	   boolean pc = true;
+       float ram = 8092;
+	   
+	   System.out.println ("Тип Вашего процессора " + CpuName);
        System.out.println ("У Вас " + cpu + " процессор");
-       System.out.println ("Частота Вашнго процессора " + frequence); 
+       System.out.println ("Частота Вашнго процессора " + frequence + " ГГЦ"); 
        System.out.println ("У Вашего процессора " + core + " ядер");
-    }
+	   System.out.println ("Тип энергопотребления Вашего процессора " + energy);
+	   System.out.println ("Емкость Вашего жесткости диска " + capacity + " MB");
+	   System.out.println ("Разрядность шины Ваего процессора " + bus + " бит");
+	   System.out.println ("Объем ОЗУ Вашего компьютера " + ram + " Гб");
+	   System.out.println ("У Вас ноутбук " + pc);
+	}
 }
