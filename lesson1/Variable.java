07a4e312fa72a5b7b07b0ccd3dc41d3622809401
am-arth <72318136+am-arth@users.noreply.github.com>
2020-12-17 10:46:25 +0300

    Add files via upload

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -1,23 +1,31 @@
-public class Variable {	
-    public static void main(String[] args) {
-       int cpu = 1;
-       int core = 4;
-       double frequence = 1.700;
-       String CpuName = "i5-3317";
-       char energy = 'U';
-	   long capacity = 976759996;
-	   byte bus = 64;
-	   boolean pc = true;
-       float ram = 8092;
-	   
-	   System.out.println ("Тип Вашего процессора " + CpuName);
-       System.out.println ("У Вас " + cpu + " процессор");
-       System.out.println ("Частота Вашнго процессора " + frequence + " ГГЦ"); 
-       System.out.println ("У Вашего процессора " + core + " ядер");
-	   System.out.println ("Тип энергопотребления Вашего процессора " + energy);
-	   System.out.println ("Емкость Вашего жесткости диска " + capacity + " MB");
-	   System.out.println ("Разрядность шины Ваего процессора " + bus + " бит");
-	   System.out.println ("Объем ОЗУ Вашего компьютера " + ram + " Гб");
-	   System.out.println ("У Вас ноутбук " + pc);
+public class Variable1 {	
+	public static void main(String[] args) {
+
+		String cpuname = "i5-3317";
+		System.out.println ("Тип Вашего процессора " + cpuname);
+
+		short cpu = 1;
+		System.out.println ("У Вас " + cpu + " процессор");
+
+		double frequence = 1.700;
+		System.out.println ("Частота Вашнго процессора " + frequence + " ГГЦ"); 
+
+		int core = 4;
+		System.out.println ("У Вашего процессора " + core + " ядер");
+
+		char energy = 'U';
+		System.out.println ("Тип энергопотребления Вашего процессора " + energy);
+
+		long capacity = 976759996;
+		System.out.println ("Емкость Вашего жесткости диска " + capacity + " MB");
+
+		byte bus = 64;
+		System.out.println ("Разрядность шины Ваего процессора " + bus + " бит");
+
+		float ram = 8092;
+		System.out.println ("Объем ОЗУ Вашего компьютера " + ram + " Гб");
+
+		boolean pc = true;
+		System.out.println ("У Вас ноутбук " + pc);
 	}
 }
