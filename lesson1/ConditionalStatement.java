7a7f4d578003afc5e70c5064d3e423189e2383be
am-arth <72318136+am-arth@users.noreply.github.com>
2020-12-20 21:04:57 +0300

    Add files via upload

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -1,35 +1,33 @@
 public class ConditionalStatement {	
-	public static void main (String[] args) {
+	public static void main(String[] args) {
+				
 		int age = 25;
+		if (age > 20) {
+		System.out.println ("старше 20");
+		} 
+		
 		char gender = 'M';
+		if (gender == 'M') {
+		System.out.println ("мужчина");
+		}
+		
+		char gender = 'F'
+		if (gender != 'M') {
+		System.out.println ("женщина");
+		}
+		
 		int height = 175;
+		if (height < 180) {
+		System.out.println("ниже 1.80");
+		} else {
+		System.out.println("выше 1.80");
+		}
+		
 		char name = 'M';
-		
-			if (age > 20) {
-				System.out.println ("старше 20");
-			} 
-		
-			if (gender == 'M') {
-				System.out.println ("мужчина");
-			}
-		
-			if (gender != 'M') {
-				System.out.println ("женщина");
-			}
-		
-			if (height < 180) {
-				System.out.println("ниже 1.80");
-			}
-
-			else {
-				System.out.println("выше 1.80");
-			}
-		
-			if (name == 'M') {
-				System.out.println ("есть такая буква в имени"); 
-			}
-			else {
-				System.out.println ("нет такой буквы в имени");
+		if (name == 'M') {
+		System.out.println ("есть такая буква в имени"); 
+		} else {
+		System.out.println ("нет такой буквы в имени");
 		}	
 	}
 }
