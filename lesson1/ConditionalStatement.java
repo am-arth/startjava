a52c1851cf29e1913e146cf0aeeb68c4ea8c1037
am-arth <72318136+am-arth@users.noreply.github.com>
2020-12-21 20:33:33 +0300

    Add files via upload

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -1,33 +1,33 @@
 public class ConditionalStatement {	
 	public static void main(String[] args) {
-				
 		int age = 25;
 		if (age > 20) {
-		System.out.println ("старше 20");
+			System.out.println ("старше 20");
 		} 
 		
 		char gender = 'M';
 		if (gender == 'M') {
-		System.out.println ("мужчина");
+			System.out.println ("мужчина");
 		}
 		
-		char gender = 'F'
-		if (gender != 'M') {
-		System.out.println ("женщина");
+		char g = 'F';
+		if (g != 'M') {
+			System.out.println ("женщина");
 		}
 		
 		int height = 175;
 		if (height < 180) {
-		System.out.println("ниже 1.80");
+			System.out.println("ниже 1.80");
 		} else {
-		System.out.println("выше 1.80");
+			System.out.println("выше 1.80");
 		}
 		
-		char name = 'M';
+		String s = "Maxim";
+		char name = s.charAt(0);
 		if (name == 'M') {
-		System.out.println ("есть такая буква в имени"); 
+			System.out.println ("есть такая буква в имени"); 
 		} else {
-		System.out.println ("нет такой буквы в имени");
+			System.out.println ("нет такой буквы в имени");
 		}	
 	}
 }
