853617092688aa7c06e457cb35cb0cd5aa6e0e7b
am-arth <72318136+am-arth@users.noreply.github.com>
2020-12-23 12:31:27 +0300

    Add files via upload

--- lesson1/Cycle.java
+++ lesson1/Cycle.java
@@ -1,14 +1,14 @@
 public class Cycle {	
 	public static void main(String[] args) {
 		for (int i = 1; i <= 20; i++) {
-		System.out.print(i + " ");
+			System.out.print(i + " ");
 		}
  		System.out.println(" ");
  		//строка чтобы отделить блоки вывода данных
  		int i = 8;
 		while (i > -6) {
 			i -= 2;
-		System.out.print(i + " ");
+			System.out.print(i + " ");
 		}
 		System.out.println(" ");
  		//строка чтобы отделить блоки вывода данных
@@ -17,7 +17,7 @@ public class Cycle {
 		do {
 			k += 2;   
 			sum += k;
-			} while(k < 19);
+		} while(k < 19);
 		System.out.print(sum + " ");
 	}
 }
