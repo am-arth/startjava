3a0e314ffe44b639405948c2a199ac6a1eb441cf
am-arth <72318136+am-arth@users.noreply.github.com>
2021-01-28 21:55:50 +0300

    Add files via upload

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -7,50 +7,40 @@ public class Calculator {
 		int a = sc.nextInt();
 		sc.nextLine();
 		System.out.println("Введите действие: +, -, *, /, ^, % ");
-		String sign = sc.nextLine();
-		//char sign = sc.nextLine().charAt(0);
+		char sign = sc.nextLine().charAt(0);
 		System.out.println("Введите 2-ое число:");
 		int b = sc.nextInt();
 		sc.close();
-		char toDo = sign.charAt(0);
-		//char toDo = sc.nextLine().charAt(0);
 
 		int result = 0;
 		int i = 0;
 		int j = 1;
 		//b = b - 1;
-		if (toDo == '^') {
-				while (i <= b - 1) {
-					i ++;
-					j = j * a;
-				}
+		if (sign == '^') {
+				for (i = 0; i <= b - 1; i++) {
+				j = j * a;
+			}
 				result = j;
 		}
-		if (toDo == '%') {
-			a = a / 100;	
-			result = a * b;
+		if (sign == '%') {	
+			result = a % b;
 		}	
-		if (toDo == '+') {
+		if (sign == '+') {
 			result = a + b;
 		}
-		if (toDo == '-') {
+		if (sign == '-') {
 			result = a - b;
 		}
-		if (toDo == '*') {
+		if (sign == '*') {
 			result = a * b;
 		}
-		if (toDo == '/') {
+		if (sign == '/') {
 			if (b <= 0) {
-				result = -1;
+			System.out.println("Делить на 0 или отрицательное число нельзя");	
 			} else {
 				result = a / b;
 				}
 		}	
-		if (result == -1 ) {
-			System.out.println("Делить на 0 или отрицательное число нельзя");	
-		} else {
 			System.out.println("Получен результат:" + result);
 		}
 	}
-}
-	
