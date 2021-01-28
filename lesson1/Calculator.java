0d623b923039a9b1ff5e218f937766daed0bc82d
am-arth <72318136+am-arth@users.noreply.github.com>
2021-01-28 11:04:01 +0300

    Add files via upload

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
+
 public class Calculator {	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int result = 0;
 		System.out.println("Введите 1-ое число:");
 		int a = sc.nextInt();
 		sc.nextLine();
 		System.out.println("Введите действие: +, -, *, /, ^, % ");
-		String phrase1 = sc.nextLine();
+		String sign = sc.nextLine();
+		//char sign = sc.nextLine().charAt(0);
 		System.out.println("Введите 2-ое число:");
 		int b = sc.nextInt();
 		sc.close();
-		char toDo = phrase1.charAt(0);
-		
+		char toDo = sign.charAt(0);
+		//char toDo = sc.nextLine().charAt(0);
+
+		int result = 0;
 		int i = 0;
 		int j = 1;
-		b = b - 1;
+		//b = b - 1;
 		if (toDo == '^') {
-				while (i <= b) {
+				while (i <= b - 1) {
 					i ++;
 					j = j * a;
 				}
 				result = j;
-		}	
+		}
 		if (toDo == '%') {
 			a = a / 100;	
 			result = a * b;
@@ -47,7 +50,7 @@ public class Calculator {
 			System.out.println("Делить на 0 или отрицательное число нельзя");	
 		} else {
 			System.out.println("Получен результат:" + result);
-			}
+		}
 	}
 }
 	
