ee68f06874ec0dff231937a35ce10e9d90989241
am-arth <72318136+am-arth@users.noreply.github.com>
2021-02-04 21:29:15 +0300

    Add files via upload

--- lesson1/MyFirstGame.java
+++ lesson1/MyFirstGame.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
-public class MyFirstGame{	
-	public static void main(String[] args) {
+public class MyFirstGame {	
+	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Игра началась, введите любое число от 0 до 100");
-		int GamerNumber = sc.nextInt();
+		System.out.println ("Игра началась, введите любое число от 0 до 100");
+		int gamerNumber = sc.nextInt();
 		sc.nextLine();
-		int ComputerNamber = 30;
-		System.out.println(GamerNumber);
-		if (GamerNumber > ComputerNamber) {
-			System.out.println("Ваше число больше, загаданного компьютером");
-			} else if (GamerNumber < ComputerNamber) {
-				System.out.println("Ваше число меньше, загаданного компьютером");
-			} else if (GamerNumber == ComputerNamber) {
-				System.out.println("Поздравляю!!! Вы выиграли, Ваше число:" + ComputerNamber);
+		int computerNumber = 30;
+		while (gamerNumber != computerNumber) {
+			if (gamerNumber > computerNumber) {
+				System.out.println ("Ваше число больше, загаданного компьютером" + gamerNumber);
+				gamerNumber--;
+					} else {
+						System.out.println ("Ваше число меньше, загаданного компьютером" + gamerNumber);
+						gamerNumber++;
+					}
 			}
+		System.out.println("Поздравляю!!! Вы выиграли, Ваше число:" + computerNumber);
 	}
 }	
